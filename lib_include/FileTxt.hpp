e31689013d737dcaaf93d2f265fe99ac85a69b35
Ptyg <nawkking@gmail.com>
2022-05-17 12:57:47 +0300

    feat: add inner class for output result

--- lib_include/FileTxt.hpp
+++ lib_include/FileTxt.hpp
@@ -22,6 +22,24 @@ public:
 	/* function takes a function that searches files in dir. Example of that func in getDirectoryFiles.cpp*/
 	void findWord(const std::function<std::vector<std::string>(const fs::path& dir, 
 															   const std::vector<std::string>& ext)>& func);
+
+	struct OutResult{
+		OutResult() = default;
+		
+		OutResult(std::string&& findWord, std::string&& file, 
+				  std::string&& line, int&& lineNumber)
+		: _findWord(std::move(findWord)), _currentFile(std::move(file)), 
+		_line(std::move(line)), _lineNumber(std::move(lineNumber)) {}
+		
+		OutResult(const std::string& findWord, const std::string& file,
+				  const std::string& line, const int& lineNumber) 
+		: _findWord(findWord), _currentFile(file), _line(line), _lineNumber(lineNumber) {}
+
+		std::string _findWord;
+		std::string _currentFile;
+		std::string _line;
+		int _lineNumber;
+	};
 };
 
 #endif
