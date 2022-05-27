66bb8644195c099522b0695ddf6351d7ebc84e86
Ptyg <nawkking@gmail.com>
2022-05-27 15:33:04 +0300

    feat: add test to check get_word

--- tests/test_txt.cpp
+++ tests/test_txt.cpp
@@ -58,3 +58,59 @@ TEST(fileTxt, getFileTypeWithFullMoveConstr_ReturnFileType_TxtFileTypeReturned){
     
     EXPECT_EQ(ftxt.getFileType(), ".txt");
 }
+
+TEST(fileTxt, getWordWithBasicConstr_ReturnEmptyWord_EmptyStringReturned){
+    FileTxt ftxt;
+    
+    EXPECT_EQ(ftxt.getFindWord(), "");
+}
+
+TEST(fileTxt, getWordWithPathMoveConstr_ReturnEmptyWord_EmptyStringReturned){
+    std::filesystem::path path{"C:\\Dir"};
+
+    FileTxt ftxt(std::move(path));
+    
+    EXPECT_EQ(ftxt.getFindWord(), "");
+}
+
+TEST(fileTxt, getWordWithPathCopyConstr_ReturnEmptyWord_EmptyStringReturned){
+    std::filesystem::path path{"C:\\Dir"};
+
+    FileTxt ftxt(path);
+    
+    EXPECT_EQ(ftxt.getFindWord(), "");
+}
+
+TEST(fileTxt, getWordWithWordMoveConstr_ReturnSetWord_SetStringReturned){
+    std::string word{"word"};
+
+    FileTxt ftxt(std::move(word));
+    
+    EXPECT_EQ(ftxt.getFindWord(), "word");
+}
+
+TEST(fileTxt, getWordWithWordCopyConstr_ReturnSetWord_SetStringReturned){
+    std::string word{"word"};
+
+    FileTxt ftxt(word);
+    
+    EXPECT_EQ(ftxt.getFindWord(), "word");
+}
+
+TEST(fileTxt, getWordWithFullMoveConstr_ReturnSetWord_SetWordReturned){
+    std::filesystem::path path{"C:\\Dir"};
+    std::string word{"word"};
+
+    FileTxt ftxt(std::move(word), std::move(path));
+    
+    EXPECT_EQ(ftxt.getFindWord(), "word");
+}
+
+TEST(fileTxt, getWordWithFullCopyConstr_ReturnSetWord_SetWordReturned){
+    std::filesystem::path path{"C:\\Dir"};
+    std::string word{"word"};
+
+    FileTxt ftxt(word, path);
+    
+    EXPECT_EQ(ftxt.getFindWord(), "word");
+}
