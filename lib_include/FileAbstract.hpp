2da704750ab997574f794b493d5e37223eec3ea4
Ptyg <nawkking@gmail.com>
2022-06-21 11:56:42 +0300

    delete: delete getters & setters

--- lib_include/FileAbstract.hpp
+++ lib_include/FileAbstract.hpp
@@ -13,22 +13,10 @@ class FileAbstract {
 public:
 	virtual ~FileAbstract() = default;
 
-	std::string getFileType() const;
-	std::string getFindWord() const;
-	std::filesystem::path getDirPath() const;
-
-	void setPath(const std::filesystem::path& path);
-	void setPath(std::filesystem::path&& path);
-	void setFileType(std::string&& type);
-	void setFileType(const std::string& type);
-	void setFindWord(std::string&& word);
-	void setFindWord(const std::string& word);
-
 	/* function takes a function that searches files in dir. Example of that func in getDirectoryFiles.cpp*/
 	std::vector<std::filesystem::path> collectFiles(const std::function<std::vector<std::filesystem::path>(
-													const std::filesystem::path& dir, 
-													const std::vector<std::string>& ext)>& func);
-
+															const std::filesystem::path& dir, 
+															const std::vector<std::string>& ext)>& func);
 };
 
 #endif
--- lib_src/FileAbstract.cpp
+++ lib_src/FileAbstract.cpp
@@ -4,48 +4,13 @@
 #include <iostream>
 #include <memory>
 
-std::filesystem::path FileAbstract::getDirPath() const{
-	return _dirPath;
-}
-
-std::string FileAbstract::getFileType() const{ 
-	return _fileType; 
-}
-
-std::string FileAbstract::getFindWord() const { 
-	return _word; 
-}
-
-void FileAbstract::setFileType(const std::string& type){
-	_fileType = type;
-}
-
-void FileAbstract::setFindWord(const std::string& word){ 
-	_word = word; 
-}
-
-void FileAbstract::setFindWord(std::string&& word){ 
-	_word = std::move(word);
-}
-
-void FileAbstract::setFileType(std::string&& type) { 
-	_fileType = std::move(type); 
-}
-
-void FileAbstract::setPath(const std::filesystem::path& path){
-	_dirPath = path;
-}
-
-void FileAbstract::setPath(std::filesystem::path&& path){
-	_dirPath = std::move(path);
-}
 
 std::vector<std::filesystem::path> FileAbstract::collectFiles(const std::function<std::vector<std::filesystem::path>(
-															  		const std::filesystem::path& dir, 
-															  		const std::vector<std::string>& ext)>& func)
+															  const std::filesystem::path& dir, 
+															  const std::vector<std::string>& ext)>& func)
 {
 	std::cout << "[INFO]: Collecting files...\n";
-	const auto files = func(getDirPath(), { getFileType() });
+	const auto files = func(_dirPath, { _fileType });
 	std::cout << "[INFO]: Collecting has been completed. Number of files: " << files.size() << '\n';
 	return files;
 }
