386afde6418606c4b430d2da2fdf2183a384cb24
Gtyp <gtyp_local@proton.me>
2023-01-20 17:28:24 +0200

    fix: make class simplier

--- lib_include/outResultBase.hpp
+++ lib_include/outResultBase.hpp
@@ -8,25 +8,15 @@
 Basic data structure for result that we get from finding word
 */
 class OutResult{
-	std::string _findWord;
-	std::filesystem::path _filePath;
-	std::string _line;
-	int _lineNumber;
 public:
-    OutResult() = default;
+	std::string m_findWord;
+	std::string m_filePath;
+	std::string m_line;
+	int m_lineNumber;
 		
-	OutResult(std::string&& findWord, std::filesystem::path&& file, 
-			  std::string&& line, int&& lineNumber)
-	: _findWord(std::move(findWord)), _filePath(std::move(file)), 
-	_line(std::move(line)), _lineNumber(std::move(lineNumber)) {}
-	
-	OutResult(const std::string& findWord, const std::filesystem::path& file,
-			  const std::string& line, const int& lineNumber) 
-	: _findWord(findWord), _filePath(file), _line(line), _lineNumber(lineNumber) {}
-
-    std::string getFindWord() const;
-    std::filesystem::path getfilePath() const;
-    std::string	getLine() const;
-    int getLineNumber() const;
+	OutResult(std::string&& findWord, std::string&& file, 
+			  std::string&& line, int lineNumber)
+	: m_findWord(std::move(findWord)), m_filePath(std::move(file)), 
+	m_line(line), m_lineNumber(std::move(lineNumber)) {}
 };
 #endif
