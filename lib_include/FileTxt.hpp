1ed1275794e262f6640f07b8f301fa65be7b9b95
Gtyp <gtyp_local@proton.me>
2023-01-20 17:35:18 +0200

    fix: simplify class

--- lib_include/FileTxt.hpp
+++ lib_include/FileTxt.hpp
@@ -6,15 +6,7 @@
 
 class FileTxt : public FileAbstract {
 public:
-	FileTxt();
-	FileTxt(const std::string& findingWord);
-	FileTxt(std::string&& findingWord);
-	FileTxt(const std::filesystem::path& path);
-	FileTxt(std::filesystem::path&& path);
-	FileTxt(const std::string& findingWord, const std::filesystem::path& path);
-	FileTxt(const std::string& findingWord, std::filesystem::path&& path);
-	FileTxt(std::string&& findingWord, std::filesystem::path&& path);
-	FileTxt(std::string&& findingWord, const std::filesystem::path& path);
+	FileTxt(std::string_view findingWord, std::string_view path);
 	~FileTxt() = default;
 
 	/* function takes a function that searches files in dir. Example of that func in getDirectoryFiles.cpp*/
