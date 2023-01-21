8f68be6bd46bff26c86306d5f31fba360cfc6aaf
Gtyp <gtyp_local@proton.me>
2023-01-21 18:59:47 +0200

    fix: make class simplier

--- lib_include/outResultXml.hpp
+++ lib_include/outResultXml.hpp
@@ -10,22 +10,15 @@
 Data structure for result that we get from finding word in xmls
 */
 class OutResultXml : public OutResult {
-    std::vector<std::string> _objects;
-    std::string _wordObject;
 public:
-    OutResultXml() = default;
-    OutResultXml(std::string&& findWord, std::filesystem::path&& file, std::vector<std::string>&& objects, 
-                 std::string&& wordObject, std::string&& line, int&& lineNumber) 
-        : OutResult(std::move(findWord), std::move(file), std::move(line), std::move(lineNumber)), 
-        _objects(std::move(objects)), _wordObject(std::move(wordObject)){}
+    std::vector<std::string> m_objects;
+    std::string m_wordObject;
 
-    OutResultXml(const std::string& findWord, const std::filesystem::path& file, const std::vector<std::string>& objects, 
-                 const std::string& wordObject, const std::string& line, const int& lineNumber) 
-        : OutResult(findWord, file, line, lineNumber),
-        _objects(objects), _wordObject(wordObject){}
-
-    std::vector<std::string> getObjects() const;
-    std::string getWordObject() const;
+    OutResultXml(std::string&& findWord, std::string&& file, 
+                 std::vector<std::string>&& objects, std::string&& wordObject, 
+                 std::string&& line, int lineNumber) 
+        : OutResult(std::move(findWord), std::move(file), std::move(line), lineNumber), 
+        m_objects(std::move(objects)), m_wordObject(std::move(wordObject)) {}
 };
 
 #endif
