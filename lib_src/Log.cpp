006ea9114ae3a70f5008e1c59a83f2242405e74f
Gtyp <gtyp_local@proton.me>
2023-01-22 16:34:19 +0200

    fix: remove default value from definition

--- lib_src/Log.cpp
+++ lib_src/Log.cpp
@@ -1,7 +1,9 @@
 #include "Log.hpp"
 #include <format>
 
+
+
 void Log::console_log(std::string_view msg, 
-                      std::string_view tag = "[INFO]") {
+                      std::string_view tag) {
     std::cout << std::format("{} {}\n", tag, msg); 
 }
