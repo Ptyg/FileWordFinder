b86a4071d4238937e25bd71a65709e0494d97efb
Gtyp <gtyp_local@proton.me>
2023-01-22 16:04:47 +0200

    fix: delete unused files

--- lib_src/outResultXml.cpp
+++
@@ -1,9 +0,0 @@
-#include "outResultXml.hpp"
-
-std::vector<std::string> OutResultXml::getObjects() const{
-    return _objects;
-}
-
-std::string OutResultXml::getWordObject() const{
-    return _wordObject;
-}
