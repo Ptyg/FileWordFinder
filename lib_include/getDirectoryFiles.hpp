1a21cb8336a4d78e92f64c1f17d0709a064e6b01
Gtyp <gtyp_local@proton.me>
2023-01-24 12:39:35 +0200

    fix: fix bug with template function definition

--- lib_include/getDirectoryFiles.hpp
+++ lib_include/getDirectoryFiles.hpp
@@ -1,6 +1,26 @@
+#include "Log.hpp"
+
 #include <vector>
+#include <string_view>
 #include <filesystem>
 
-template<typename iterator, typename path, typename iteratable_vect>
-std::vector<std::filesystem::path> getDirectoryFiles(path&& dir, iteratable_vect&& ext);
+template<typename iterator = std::filesystem::directory_iterator,
+         typename path = std::string_view, 
+         typename iteratable_vect = std::vector<std::string_view>>
+std::vector<std::filesystem::path> getDirectoryFiles(path&& dir, iteratable_vect&& ext) {
+	std::vector<std::filesystem::path> files;
+		
+	try{
+		for (const auto& p : iterator(dir)) {
+			if (std::filesystem::is_regular_file(p)) {
+				if (ext.empty() || find(ext.begin(), ext.end(), p.path().extension().string()) != ext.end())
+					files.push_back(p.path());
+			}
+		}
+	}
+	catch (const std::exception& ex){
+		Log::console_log(ex.what(), "[ERROR]");
+	}
+	return files;
+}
 
